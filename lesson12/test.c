572264a7f5a914cf663e58e4a0b62b674dccba43
qkja <2660015902@qq.com>
2021-09-28 17:47:32 +0600

    fisr

---
+++ lesson12/test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+
+  int a = 10;
+  int d = 20;
+  printf("please scanf the a of value>:");
+  scanf("%d",&a);
+  printf("%d\n",a);
+  return 0;
+}
---
+++ lesson13/test.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+
+  /*bool ret = false;
+  if(!ret)
+  {
+    printf("hehehehe");
+  }*/ 
+
+  /*float f = 0.0;
+  int a = 0;
+  if(a==f)
+  {
+    printf("hello\n");
+  }
+  // else 
+ //   printf("hahaha\n");*/ 
+
+  //printf("%d\n",sizeof(int)); 
+  // printf("%d\n",sizeof(char));
+  //float f = 3.14;
+  //int a;
+  //printf("%d\n",a);
+  //printf("%d\n",a);
+  return 0;
+}
