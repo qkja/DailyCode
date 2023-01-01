3e8c1fbd21cd18f1216f24543164e98b752c6943
算法杰哥 <qkj1205@163.com>
2023-01-01 23:25:38 +0800

    左神 2022 第一题 多叉树

---
+++ 2023_01_01/test.cc
@@ -0,0 +1,46 @@
+#include <vector>
+using std::vector;
+// 给定一个多叉树,该多叉树黑色节点所在路径都进行保留
+// 放回新的多叉树的节点
+/// @brief 由于是多叉树,我不知道如何表示
+struct Node
+{
+  bool retain; // 是否保留
+  vector<Node*> nexts; // 子节点的地址
+  int val; // 当前节点的值
+  Node(int v,bool r)
+  {
+    val = v;
+    retain = r;
+  }
+};
+
+Node* retain(Node* head)
+{
+  if(head->nexts.empty())
+  {
+    return head->retain?head:nullptr;
+  }
+  //head下层有节点 我们便利所有的节点
+  vector<Node*> v;
+  for(Node* p:head->nexts)
+  {
+    Node* newNext = retain(p);
+    if(newNext != nullptr)
+    {
+      v.push_back(newNext);
+    }
+  }
+
+  // newNext 是新的链表  nexts是老的链表
+  if(!v.empty() || head->retain)
+  {
+    head->nexts = v;
+    return head;
+  }
+  return nullptr;
+}
+
+
+
+// 猜数字游戏   笔试整理 第二题
