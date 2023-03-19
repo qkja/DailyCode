35d9df04beb7ed9bb497ec7c3942eaacd0a58822
算法杰哥 <qkj1205@163.com>
2023-03-19 16:45:21 +0800

    力扣 628 三个数的最大乘积

--- 2023_03_19/test.cc
+++ 2023_03_19/test.cc
@@ -5,7 +5,24 @@
  * Time: 15:27
  */
 #include <vector>
+#include <algorithm>
 using namespace std;
+class Solution
+{
+public:
+  int maximumProduct(vector<int> &nums)
+  {
+    if (nums.size() < 3)
+    {
+      return -1;
+    }
+    sort(nums.begin(), nums.end(), greater<int>()); // 默认是升序  ,这里我们改成降序
+    int max1 = nums[0] * nums[1] * nums[2]; // 都是整数,或者都是负数
+    int max2 = nums[0] * nums[nums.size() - 2] * nums[nums.size() - 1]; // 正数和负数混在
+    return max(max1, max2);
+  }
+};
+
 /// @brief 种花问题
 // class Solution
 // {
@@ -107,44 +124,44 @@ using namespace std;
 //     // }
 //   };
 
-  // class Solution
-  // {
-  // private:
-  //   bool canPlaceFlowersChild(vector<int> &flowerbed, int left, int right, int n)
-  //   {
-  //     if(left > right)
-  //     {
-  //       return false;
-  //     }
-  //     if (n == 0 && left <= right)
-  //     {
-  //       return true;
-  //     }
-  //     if (flowerbed[left] == 0)
-  //     {
-  //       // 此时判断 左侧和右侧有没有 花
-  //       int i = 0;
-  //       int j = 0;
-  //       if (left - 1 >= 0)
-  //         i = left - 1;
+// class Solution
+// {
+// private:
+//   bool canPlaceFlowersChild(vector<int> &flowerbed, int left, int right, int n)
+//   {
+//     if(left > right)
+//     {
+//       return false;
+//     }
+//     if (n == 0 && left <= right)
+//     {
+//       return true;
+//     }
+//     if (flowerbed[left] == 0)
+//     {
+//       // 此时判断 左侧和右侧有没有 花
+//       int i = 0;
+//       int j = 0;
+//       if (left - 1 >= 0)
+//         i = left - 1;
 
-  //       j = left + 1;
-  //       if (left + 1 > right)
-  //         j = right;
-  //       if (flowerbed[i] == 0 && flowerbed[j] == 0)
-  //       {
-  //         // 此时可以了
-  //         flowerbed[left] = 1;
-  //         n--;
-  //       }
-  //     }
-  //     return canPlaceFlowersChild(flowerbed, ++left, right, n);
-  //   }
+//       j = left + 1;
+//       if (left + 1 > right)
+//         j = right;
+//       if (flowerbed[i] == 0 && flowerbed[j] == 0)
+//       {
+//         // 此时可以了
+//         flowerbed[left] = 1;
+//         n--;
+//       }
+//     }
+//     return canPlaceFlowersChild(flowerbed, ++left, right, n);
+//   }
 
-  // public:
-  //   bool canPlaceFlowers(vector<int> &flowerbed, int n)
-  //   {
+// public:
+//   bool canPlaceFlowers(vector<int> &flowerbed, int n)
+//   {
 
-  //     return canPlaceFlowersChild(flowerbed, 0, flowerbed.size() - 1, n);
-  //   }
-  // };
+//     return canPlaceFlowersChild(flowerbed, 0, flowerbed.size() - 1, n);
+//   }
+// };
