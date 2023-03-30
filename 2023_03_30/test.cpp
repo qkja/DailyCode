d933ac3aeb2ffdf34c56652e19052390e0052a97
algorithmic-jacko <qkj1205@163.com>
2023-03-30 22:47:22 +0800

    阿秀的笔记 链表所有

--- 2023_03_30/test.cpp
+++ 2023_03_30/test.cpp
@@ -3,50 +3,189 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-class Solution {
-public:
-    /**
-     *
-     * @param s string字符串
-     * @return int整型
-     */
-    bool is_reply(std::string& str, int left, int right)
-    {
-        while (left < right)
-        {
-            if (str[left++] != str[right--])
-                return false;
-        }
-        return true;
+//struct ListNode 
+//{
+//    int val;
+//    ListNode* next;
+//    ListNode() : val(0), next(nullptr) {}
+//    ListNode(int x) : val(x), next(nullptr) {}
+//    ListNode(int x, ListNode* next) : val(x), next(next) {}
+//};
+//
+//
+//
+//class Solution {
+//public:
+//    void deleteNode(ListNode* node) {
+//        if (nullptr == node || node->next == nullptr)
+//            return;
+//        ListNode* next = node->next;
+//        while (next->next)
+//        {
+//            node->val = next->val;
+//            node = next;
+//            next = next->next;
+//        }
+//        // 覆盖一下
+//        node->val = next->val;
+//        node->next = nullptr;
+//        delete next;
+//    }
+//};
 
-    }
 
-    int minCut(string s) {
-        if (s.empty())
-            return 0;
 
-        int len = s.size();
-        std::vector<int> dp(len + 1, 0);
 
-        for (int i = 2; i < len + 1; i++)
-        {
-            if (!is_reply(s, 0, i - 1))
-            {
-                int ret = dp[i - 1] + 1;
-                for (int j = 1; j < i; j++)
-                {
-                    if (is_reply(s, j, i - 1))
-                    {
-                        ret = std::min(ret, dp[j] + 1);
-                    }
-                }
-                dp[i] = ret;
-            }
-        }
 
-        return dp[len];
-    }
-};
+
+
+
+
+
+
+//struct Solution {
+//public:
+//    ListNode* reverseList(ListNode* head) {
+//        if (head == nullptr)
+//            return nullptr;
+//        ListNode* prev = nullptr;
+//        ListNode* node = head;
+//        ListNode* next = head->next;
+//        while (node)
+//        {
+//            next = node->next; // 保存 下一个节点
+//
+//            node->next = prev;
+//            prev = node;
+//            node = next;
+//        }
+//        return prev;
+//    }
+//};
+//
+//int main()
+//{
+//    int a = 0;
+//    a = 10;
+//    return 0;
+//}
+
+
+
+//class Solution {
+//public:
+//    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//        if (list1 == nullptr)
+//            return list2;
+//        if (list2 == nullptr)
+//            return list1;
+//        ListNode* head = nullptr;
+//        ListNode* tail = head;
+//        while (list1 && list2)
+//        {
+//            if (list1->val <= list2->val)
+//            {
+//                if (head == nullptr)
+//                {
+//                    head = list1;
+//                    tail = head;
+//                }
+//                else
+//                {
+//                    tail->next = list1;
+//                    tail = tail->next;
+//                }
+//                list1 = list1->next;
+//            }
+//            else
+//            {
+//                if (head == nullptr)
+//                {
+//                    head = list2;
+//                    tail = head;
+//                }
+//                else
+//                {
+//                    tail->next = list2;
+//                    tail = tail->next;
+//                }
+//                list2 = list2->next;
+//            }
+//        }
+//        if (list1)
+//        {
+//            tail->next = list1;
+//        }
+//        if (list2)
+//        {
+//            tail->next = list2;
+//
+//        }
+//        return head;
+//    }
+//};
+
+
+//class Solution {
+//public:
+//    /**
+//     *
+//     * @param word1 string字符串
+//     * @param word2 string字符串
+//     * @return int整型
+//     */
+//    int minDistance(string word1, string word2) {
+//        // write code here
+//    }
+//};
+
+
+
+
+//class Solution {
+//public:
+//    /**
+//     *
+//     * @param s string字符串
+//     * @return int整型
+//     */
+//    bool is_reply(std::string& str, int left, int right)
+//    {
+//        while (left < right)
+//        {
+//            if (str[left++] != str[right--])
+//                return false;
+//        }
+//        return true;
+//
+//    }
+//
+//    int minCut(string s) {
+//        if (s.empty())
+//            return 0;
+//
+//        int len = s.size();
+//        std::vector<int> dp(len + 1, 0);
+//
+//        for (int i = 2; i < len + 1; i++)
+//        {
+//            if (!is_reply(s, 0, i - 1))
+//            {
+//                int ret = dp[i - 1] + 1;
+//                for (int j = 1; j < i; j++)
+//                {
+//                    if (is_reply(s, j, i - 1))
+//                    {
+//                        ret = std::min(ret, dp[j] + 1);
+//                    }
+//                }
+//                dp[i] = ret;
+//            }
+//        }
+//
+//        return dp[len];
+//    }
+//};
 
 
 
