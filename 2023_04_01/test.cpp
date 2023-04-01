00cc13588e5918867b0d6a1ed487fd290f7ac92b
algorithmic-jacko <qkj1205@163.com>
2023-04-01 20:42:04 +0800

    最大连续子数组和 优化

--- 2023_04_01/test.cpp
+++ 2023_04_01/test.cpp
@@ -3,25 +3,51 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         if (nums.empty())
             return -1;
-        std::vector<int> dp(nums.size());
-        dp[0] = nums[0];
-        int maxSum = dp[0];
+        int left = nums[0];
+        int right = 0;
+        int maxSum = left;
         for (int i = 1; i < nums.size(); i++)
         {
-            dp[i] = std::max(dp[i - 1] + nums[i], nums[i]);
-            if (dp[i] > maxSum)
-                maxSum = dp[i];
+            if (left < 0)
+            {
+                right = nums[i];
+            }
+            else
+            {
+                right = nums[i] + left;
+            }
+            if (right > maxSum)
+                maxSum = right;
+            left = right;
         }
-        
+
         return  maxSum;
     }
 };
+
+//class Solution {
+//public:
+//    int maxSubArray(vector<int>& nums) {
+//        if (nums.empty())
+//            return -1;
+//        std::vector<int> dp(nums.size());
+//        dp[0] = nums[0];
+//        int maxSum = dp[0];
+//        for (int i = 1; i < nums.size(); i++)
+//        {
+//            dp[i] = std::max(dp[i - 1] + nums[i], nums[i]);
+//            if (dp[i] > maxSum)
+//                maxSum = dp[i];
+//        }
+//        
+//        return  maxSum;
+//    }
+//};
 //class Solution {
 //public:
 //    string convert(string s, int numRows) {
