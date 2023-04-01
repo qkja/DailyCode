ae4aeb6b964ab5e8d0972f90404afb5a0bfa1d59
algorithmic-jacko <qkj1205@163.com>
2023-04-01 20:24:11 +0800

    N字型变换

---
+++ 2023_04_01/test.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int len = s.size();
+        if (numRows == 1 || numRows >= len)
+        {
+            return s;
+        }
+
+        std::string result;
+        result.resize(len);
+        int t = 2 * numRows - 2; // 这里是周期
+        int index = 0;
+        for (int i = 0; i < numRows; i++)
+        {
+            // i 当前来到第i 行
+            for (int j = i, nextColTop = t; j < len;  nextColTop += t,j+= t)
+            {
+                result[index++] = s[j];
+                if (i >= 1 && i <= numRows - 2 && nextColTop - i < len)
+                {
+                    result[index++] = s[nextColTop-i];
+                }
+            }
+        }
+        return result;
+    }
+};
