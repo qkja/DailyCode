ec05553118fabdabe98f7b0ffee36dfadb71bcf0
qkja <qkj1205@163.com>
2023-04-12 11:10:41 +0800

    传输错了 笔试强训 day 17 选择

---
+++ 2023_04_12/2023_04_12/test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+//class Base {
+//public:
+//	Base() {
+//		echo();
+//	}
+//	virtual void echo() {
+//		printf("Base");
+//	}
+//};
+//class Derived :public Base {
+//public:
+//	Derived() {
+//		echo();
+//	}
+//	virtual void echo() {
+//		printf("Derived");
+//	}
+//};
+//int main() {
+//	Base* base = new Derived();
+//	base->echo();
+//	return 0;
+//}
