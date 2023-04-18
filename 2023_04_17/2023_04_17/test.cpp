67a2980d2830767f628bd35858590fa7ef75215c
qkja <qkj1205@163.com>
2023-04-18 22:52:46 +0800

    基数排序 直接插入排序

--- 2023_04_17/2023_04_17/test.cpp
+++ 2023_04_17/2023_04_17/test.cpp
@@ -1,15 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
-<<<<<<< HEAD
 using std::cin;
 using std::cout;
 using std::endl;
-
-=======
-#include <vector>
-#include <unordered_map>
-
-using namespace std;
+//
+//
+//
+//<<<<<<< HEAD
+//using std::cin;
+//using std::cout;
+//using std::endl;
+//
+//=======
+//#include <vector>
+//#include <unordered_map>
+//
+//using namespace std;
 //class Solution
 //{
 //public:
@@ -73,7 +79,7 @@ using namespace std;
 //        {
 //            for (size_t j = 0; j < col; j++)
 //            {
-//                if (board[i][j] == 'A')
+//           if (board[i][j] == 'A')
 //                    board[i][j] = 'O';
 //                else
 //                    board[i][j] = 'X';
@@ -93,35 +99,35 @@ using namespace std;
 //    Solution().solve(v);
 //    return 0;
 //}
-
-
-
->>>>>>> fa1279b0c55d97d5d00607e0851b37f4d57d1fb3
+//
+//
+//
+//>>>>>>> fa1279b0c55d97d5d00607e0851b37f4d57d1fb3
 //class A
 //{
 //public:
 //	void func()const
 //	{
-<<<<<<< HEAD
+//<<<<<<< HEAD
 //		std::cout << "a: " << a << std::endl;
 //		std::cout << "b: " << b << std::endl;
-=======
+//=======
 //		std::cout << 'a: ' << a << std::endl;
 //		std::cout << 'b: ' << b << std::endl;
->>>>>>> fa1279b0c55d97d5d00607e0851b37f4d57d1fb3
+//>>>>>>> fa1279b0c55d97d5d00607e0851b37f4d57d1fb3
 //		
 //		a = 20;
 //	/*	A* p = const_cast<A*>(this);
 //		p->a = 20;
 //		p->b = 20;
 //*/
-<<<<<<< HEAD
+//<<<<<<< HEAD
 //		std::cout << "a: " << a << std::endl;
 //		std::cout << "b: " << b << std::endl;
-=======
+//=======
 //		std::cout << 'a: ' << a << std::endl;
 //		std::cout << 'b: ' << b << std::endl;
->>>>>>> fa1279b0c55d97d5d00607e0851b37f4d57d1fb3
+//>>>>>>> fa1279b0c55d97d5d00607e0851b37f4d57d1fb3
 //	}
 //private:
 //	mutable int a = 10;
@@ -132,11 +138,11 @@ using namespace std;
 //{
 //	A().func();
 //	return 0;
-<<<<<<< HEAD
+//<<<<<<< HEAD
 //}
-
-#include <cstring>
-
+//
+//#include <cstring>
+//
 //char * strcpy(char * destination, const char * source);
 //char * my_strcpy(char * destination, const char * source)
 //{
@@ -172,28 +178,28 @@ using namespace std;
 //	puts(str);
 //	return 0;
 //}
-
+//
 //////////////////////////////////////////////////////////////////////////////////////
-
-
+//
+//
 // C++中的模板  -- 也就是静态多态
-
+//
 // 函数模板 --> 需要两次编译
 //template < class T1...>
 //T1 func(T1 t)
-
+//
 //说一下,我们函数模板一般是不需要指定我们的模板参数的具体类型的,毕竟我们的形参可以推导出来,
 //这里有个特殊的情况,如果我们的没有形参可以推导,此时需要显示指定例如
 //template < class T1...>
 //T1 func(int t)             -----> func<double>(1)
-
-
+//
+//
 // 类模板   --> 显示指定我们的类型
 // 自我感觉类模板的存在主要是为了我们的类中的类型存在的,毕竟我们后面的STL,注意一下我们的类模板如果里面的成员函数
 // 定义和模板进行分离,那么注意要带上类模板
-
-
-
+//
+//
+//
 // 模板参数可以缺省,不用从右往左,不过我们最好从右往左,因为下面的你无法调用
 //template<class T = int, typename K>
 //T func(int n)
@@ -207,27 +213,27 @@ using namespace std;
 //	std::cout << ret << std::endl;
 //	return 0;
 //}
-
+//
 //进阶
-
+//
 // 模板参数  --> 
 // 类型模板参数  --> 不支持隐式类型转换  --> 非常好
 // 非类型模板参数--> 
-
-
-
-
-
-
-
-
-
-
-
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 //模板特化
 // 半特化
 // 全特化
-
+//
 //template<>
 //bool Less<int*>(int* left, int* right)
 //  全特化
@@ -238,7 +244,7 @@ using namespace std;
 //class Data<int, T2>
 // 半特化
 // 半特化
-
+//
 //#define min(a,b) ((a) < (b) ? (a) : (b))
 //const int size = 10;
 //int ia[size];
@@ -252,6 +258,62 @@ using namespace std;
 //	cout << "elem_cnt : " << elem_cnt
 //		<< "\texpecting: " << size << endl;
 //	return 0;
-=======
->>>>>>> fa1279b0c55d97d5d00607e0851b37f4d57d1fb3
+//=======
+//>>>>>>> fa1279b0c55d97d5d00607e0851b37f4d57d1fb3
 //}
+//
+//
+//template <class Type, int size>
+//Type min(const Type(r_array)[size])
+//{
+//	/* 找到数组中元素最小值的参数化函数 */
+//	Type min_val = r_array[0];
+//	for (int i = 1; i < size; ++i)
+//	if (r_array[i] < min_val)
+//		min_val = r_array[i];
+//	return min_val;
+//}
+//int main()
+//{
+//	int arr[100] = { -10, 1, 2, 3, 4, 5 };
+//	int ret = min<int, 100>(arr);
+//	cout << ret << endl;
+//	return 0;
+//}
+//class A
+//{
+//public:
+//	int a = 10;
+//	static int name;
+//};
+//
+//int A::name = 10;
+//template <class Parm>
+//int minus(Parm* par)
+//{
+//   Parm::name * p; // 这是一个指针声明还是乘法乘法
+//}
+//
+//int main()
+//{
+//	A a;
+//	minus(&a);
+//	return 0;
+//}
+//
+//template <class T, class U, class V>
+//void foo(T, U, V)
+//{
+//	cout << "此时" << endl;
+//}
+//
+//int main()
+//{
+//	foo(1, 1, 1);
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
