92aceec22ff77d41ea1797d9f9396777b32c1900
qkja <qkj1205@163.com>
2023-04-23 22:57:17 +0800

    二分查找

--- 2023_04_23/test.cpp
+++ 2023_04_23/test.cpp
@@ -1,3 +1,33 @@
+#include <vector>
+using std::vector;
+
+//https://leetcode.cn/problems/binary-search/submissions/
+// 二分查找常见错误
+// 循环判断
+// left 和 right 的更新
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.empty())
+            return -1;
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return -1;
+    }
+};
+
 int main()
 {
 
