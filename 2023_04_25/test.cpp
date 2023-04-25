ec7b10156d6b9a980f4f07cf9c7863f848655946
qkja <qkj1205@163.com>
2023-04-25 22:32:01 +0800

    设计链表

--- 2023_04_25/test.cpp
+++ 2023_04_25/test.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <iostream>
+
 using std::vector;
 
 //class Solution {
@@ -50,54 +52,171 @@ using std::vector;
 //    }
 //};
 
-//https://leetcode.cn/problems/remove-linked-list-elements/submissions/
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode* next) : val(x), next(next) {}
-};
+////https://leetcode.cn/problems/remove-linked-list-elements/submissions/
+//struct ListNode {
+//    int val;
+//    ListNode* next;
+//    ListNode() : val(0), next(nullptr) {}
+//    ListNode(int x) : val(x), next(nullptr) {}
+//    ListNode(int x, ListNode* next) : val(x), next(next) {}
+//};
+//
+//class Solution {
+//public:
+//    ListNode* removeElements(ListNode* head, int val) {
+//        if (head == nullptr)
+//            return nullptr;
+//        ListNode* new_head = nullptr;
+//        ListNode* tail = nullptr;
+//        ListNode* cur = head;
+//        while (cur != nullptr)
+//        {
+//            if (cur->val != val)
+//            {
+//                if (tail == nullptr)
+//                {
+//                    new_head = cur;
+//                    tail = cur;
+//                }
+//                else
+//                {
+//                    tail->next = cur;
+//                    tail = cur;
+//                }
+//                cur = cur->next;
+//
+//            }
+//            else
+//            {
+//                // 注意，这里我们需要回收内存
+//                ListNode* node = cur;
+//                cur = cur->next;
+//                delete node;
+//            }
+//
+//        }
+//
+//        if (tail)
+//        {
+//            tail->next = nullptr;
+//        }
+//        return new_head;
+//    }
+//};
 
-class Solution {
-public:
-    ListNode* removeElements(ListNode* head, int val) {
-        if (head == nullptr)
-            return nullptr;
-        ListNode* new_head = nullptr;
-        ListNode* tail = nullptr;
-        ListNode* cur = head;
-        while (cur != nullptr)
-        {
-            if (cur->val != val)
-            {
-                if (tail == nullptr)
-                {
-                    new_head = cur;
-                    tail = cur;
-                }
-                else
-                {
-                    tail->next = cur;
-                    tail = cur;
-                }
-                cur = cur->next;
-
-            }
-            else
-            {
-                // 注意，这里我们需要回收内存
-                ListNode* node = cur;
-                cur = cur->next;
-                delete node;
-            }
-
-        }
-
-        if (tail)
-        {
-            tail->next = nullptr;
-        }
-        return new_head;
-    }
-};
+//https://leetcode.cn/problems/design-linked-list/submissions/
+//struct Node
+//{
+//    Node(int x)
+//        :val(x)
+//        ,next(nullptr)
+//    {}
+//    int val;
+//    struct Node* next;
+//};
+//
+//class MyLinkedList {
+//    
+//public:
+//    MyLinkedList() {
+//        _head = nullptr;
+//        _size = 0;
+//    }
+//
+//    int get(int index) {
+//        if(index < 0 || index >= _size)
+//            return -1;
+//
+//        Node* cur = _head;
+//        // 返回 1 的位置
+//        while (index--)
+//        {
+//            cur = cur->next;
+//        }
+//        return cur == nullptr ? -1 : cur->val;
+//    }
+//
+//    void addAtHead(int val) {
+//        Node* node = new Node(val);
+//        if (_head == nullptr)
+//        {
+//            _head = node;
+//        }
+//        else
+//        {
+//            node->next = _head;
+//            _head = node;
+//        }
+//        _size++;
+//    }
+//
+//    void addAtTail(int val) {
+//        addAtIndex(_size, val);
+//    }
+//
+//    void addAtIndex(int index, int val) {
+//        if (index < 0 || index > _size)
+//            return;
+//        if (index == 0)
+//        {
+//            // 头插
+//            addAtHead(val);
+//        }
+//        else
+//        {
+//            Node* node = new Node(val);
+//
+//            Node* prev = nullptr;
+//            Node* cur = _head;
+//            // 寻找位置
+//            while (index--)
+//            {
+//                prev = cur;
+//                cur = cur->next;
+//                if (cur == nullptr)
+//                    break;
+//            }
+//
+//            prev->next = node;
+//            node->next = cur;
+//        }
+//        _size++;
+//    }
+//
+//    void deleteAtIndex(int index) {
+//
+//        if (index < 0 || index >= _size)
+//            return;
+//        Node* cur = _head;
+//        Node* prev = nullptr;
+//
+//        while (index > 0)
+//        {
+//            prev = cur;
+//            cur = cur->next;
+//            index--;
+//        }
+//        if (prev == nullptr)
+//        {
+//            _head = _head->next;
+//            delete cur;
+//        }
+//        else
+//        {
+//            prev->next = cur->next;
+//            delete cur;
+//        }
+//        _size--;
+//    }
+//    Node* _head;
+//    int _size;
+//};
+//int main()
+//{
+//    MyLinkedList my;
+//    my.addAtTail(1);
+//    my.addAtTail(3);
+//
+//    std::cout << my.get(1) << std::endl;
+//    return 0;
+//}
