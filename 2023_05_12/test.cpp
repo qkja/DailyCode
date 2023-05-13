49f9371c66c777349ba05f50347f5ad128bfb2b3
qkja <qkj1205@163.com>
2023-05-13 22:53:59 +0800

    二叉树 OJ

--- 2023_05_12/test.cpp
+++ 2023_05_12/test.cpp
@@ -287,22 +287,6 @@ using namespace std;
 //};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // https://leetcode.cn/problems/top-k-frequent-elements/submissions/
 //class Solution {
 //public:
@@ -356,3 +340,98 @@ using namespace std;
 //    }
 //};
 
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+
+
+class Solution {
+public:
+
+    vector<int> preorderTraversal(TreeNode* root) {
+
+    }
+};
+
+
+
+//https://leetcode.cn/problems/binary-tree-preorder-traversal/submissions/
+class Solution {
+//public:
+//    // Ç°Ðò±éÀú -- > ¸ù×óÓÒ
+//    // µÝ¹é
+//    void preorderTraversalChild(TreeNode* root, std::vector<int>& result)
+//    {
+//        if (root == nullptr)
+//            return;
+//        result.push_back(root->val);
+//        preorderTraversalChild(root->left, result);
+//        preorderTraversalChild(root->right, result);
+//
+//    }
+//    // ·ÇµÝ¹é
+//    void preorderTraversalChildR(TreeNode* root, std::vector<int>& result)
+//    {
+//        if (root == nullptr)
+//            return; 
+//        std::stack<TreeNode*> s;
+//        s.push(root);
+//        TreeNode* node = nullptr;
+//        while (!s.empty())
+//        {
+//            node = s.top();
+//            s.pop();
+//            result.push_back(node->val);
+//            if (node->right)
+//                s.push(node->right);
+//            if (node->left)
+//                s.push(node->left);
+//        }
+//    }
+//    vector<int> preorderTraversal(TreeNode* root) {
+//        std::vector<int> result;
+//        //preorderTraversalChild(root, result);
+//        preorderTraversalChildR(root, result);
+//        return result;
+//    }
+//};
+//https://leetcode.cn/problems/binary-tree-postorder-traversal/submissions/
+//class Solution {
+//public:
+//    void postorderTraversalChild(TreeNode* root, std::vector<int>& result)
+//    {
+//        if (root == nullptr)
+//            return;
+//        postorderTraversalChild(root->left, result);
+//        postorderTraversalChild(root->right, result);
+//        result.push_back(root->val);
+//    }
+//    vector<int> postorderTraversal(TreeNode* root) {
+//        std::vector<int> result;
+//        postorderTraversalChild(root, result);
+//        return result;
+//    }
+//};
+//https://leetcode.cn/problems/binary-tree-inorder-traversal/submissions/
+//class Solution {
+//public:
+//    void  inorderTraversalChild(TreeNode* root, std::vector<int>& result)
+//    {
+//        if (root == nullptr)
+//            return;
+//        inorderTraversalChild(root->left, result);
+//        result.push_back(root->val);
+//        inorderTraversalChild(root->right, result);
+//    }
+//    vector<int> inorderTraversal(TreeNode* root) {
+//        std::vector<int> result;
+//        inorderTraversalChild(root, result);
+//        return result;
+//    }
+//};
