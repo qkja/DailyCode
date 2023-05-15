1eab8b1874dba4e226b433ddbca79fdefb73094f
qkja <qkj1205@163.com>
2023-05-15 13:34:48 +0800

    二叉树OJ

--- 2023_05_15/2023_05_15/test.cpp
+++ 2023_05_15/2023_05_15/test.cpp
@@ -1,1 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+struct TreeNode 
+{
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+		val(x), left(NULL), right(NULL) {
+	}
+};
+class Solution {
+public:
+	void InOrderConvert(TreeNode* cur, TreeNode*& prev)
+	{
+		if (cur == nullptr)
+			return;
+		InOrderConvert(cur->left, prev);
+		cur->left = prev;
+		if (prev)
+			prev->right = cur;
+		prev = cur; // 注意这里要给一个引用
+		InOrderConvert(cur->right, prev);
+	}
+	TreeNode* Convert(TreeNode* pRootOfTree) {
+		if (pRootOfTree == nullptr)
+			return nullptr;
+		TreeNode* prev = nullptr;
+		InOrderConvert(pRootOfTree, prev);
+
+		// 这里遍历数组
+		TreeNode* head = pRootOfTree;
+		while (head->left)
+		{
+			head = head->left;
+		}
+		return head;
+	}
+};
