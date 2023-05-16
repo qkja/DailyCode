c2801d2e2f86ab24620bfbf804818a47570d481a
qkja <qkj1205@163.com>
2023-05-16 11:39:30 +0800

    代码随想录　组合

--- 2023_05_16/test.cpp
+++ 2023_05_16/test.cpp
@@ -1,29 +1,63 @@
 #include <iostream>
-
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+#include <vector>
+using namespace std;
+// https://leetcode.cn/problems/combinations/submissions/
+class Solution {
+public:
+    void backtracking(vector<int>& path, vector<vector<int>>& result,
+        int n, int k, int index)
+    {
+        //  ≤√¥ ±∫Ú÷’÷π
+        if (path.size() == k)
+        {
+            result.push_back(path);
+            return;
+        }
+        for (size_t i = index; i <= n; i++)
+        {
+            path.push_back(i);
+            backtracking(path, result, n, k, i + 1);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        if (n <= 0 || k <= 0 || k > n)
+            return result;
+        vector<int> path;
+        backtracking(path, result, n, k, 0);
+        return result;
+    }
 };
+
+
+
+
+
+//struct TreeNode {
+//    int val;
+//    TreeNode* left;
+//    TreeNode* right;
+//    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+//};
 
 // https://leetcode.cn/problems/invert-binary-tree/submissions/
-class Solution {
-
-public:
-    void _invertTree(TreeNode* root) {
-        if (root == nullptr)
-            return;
-        std::swap(root->left, root->right);
-        _invertTree(root->left);
-        _invertTree(root->right);
-    }
-    TreeNode* invertTree(TreeNode* root) {
-        if (root == nullptr)
-            return nullptr;
-        _invertTree(root);
-        return root;
-    }
-};
+//class Solution {
+//
+//public:
+//    void _invertTree(TreeNode* root) {
+//        if (root == nullptr)
+//            return;
+//        std::swap(root->left, root->right);
+//        _invertTree(root->left);
+//        _invertTree(root->right);
+//    }
+//    TreeNode* invertTree(TreeNode* root) {
+//        if (root == nullptr)
+//            return nullptr;
+//        _invertTree(root);
+//        return root;
+//    }
+//};
