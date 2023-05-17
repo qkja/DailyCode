09902361b43e9ddba907507bea49e6a2841aeb47
qkja <qkj1205@163.com>
2023-05-17 15:24:01 +0800

    代码随想录 摆动序列

--- 2023_05_17/test.cpp
+++ 2023_05_17/test.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <unordered_map>
 using namespace std;
 
 //struct TreeNode {
@@ -35,48 +37,73 @@ using namespace std;
 //    }
 //};
 // https://leetcode.cn/problems/combination-sum-iii/submissions/
-class Solution {
-public:
-	struct Node
-	{
-		std::vector<int> v;
-		int sum = 0;
-	};
-	void backtarning(vector<vector<int>>& result,
-		Node& node, int index, int k, int n)
-	{
-		if (node.v.size() == k && node.sum == n)
-		{
-			result.push_back(node.v);
-			return;
-		}
+//class Solution {
+//public:
+//	struct Node
+//	{
+//		std::vector<int> v;
+//		int sum = 0;
+//	};
+//	void backtarning(vector<vector<int>>& result,
+//		Node& node, int index, int k, int n)
+//	{
+//		if (node.v.size() == k && node.sum == n)
+//		{
+//			result.push_back(node.v);
+//			return;
+//		}
+//
+//		for (int i = index; i < 10; i++)
+//		{
+//			// 这里我们就是裁了一部分,也可以看  我们v的数量和剩余的放在一起也有可能达不到k
+//			/*if (node.sum > n
+//				|| node.v.size() >= k
+//				|| (node.v.size() + 10 - index) < k)
+//			{
+//				continue;
+//			}*/
+//			if (node.sum > n || node.v.size() >= k)
+//				continue;
+//			node.v.push_back(i);
+//			node.sum += i;
+//			backtarning(result, node, i + 1, k, n);
+//			node.v.pop_back();
+//			node.sum -= i;
+//		}
+//	}
+//
+//    vector<vector<int>> combinationSum3(int k, int n) {
+//		vector<vector<int>> result;
+//		if (k <= 0 || n <= 0)
+//			return result;
+//		Node node;
+//		int index = 1;
+//		backtarning(result, node, index, k, n);
+//		return result;
+//    }
+//};
 
-		for (int i = index; i < 10; i++)
-		{
-			// 这里我们就是裁了一部分,也可以看  我们v的数量和剩余的放在一起也有可能达不到k
-			/*if (node.sum > n
-				|| node.v.size() >= k
-				|| (node.v.size() + 10 - index) < k)
-			{
-				continue;
-			}*/
-			if (node.sum > n || node.v.size() >= k)
-				continue;
-			node.v.push_back(i);
-			node.sum += i;
-			backtarning(result, node, i + 1, k, n);
-			node.v.pop_back();
-			node.sum -= i;
-		}
-	}
-
-    vector<vector<int>> combinationSum3(int k, int n) {
-		vector<vector<int>> result;
-		if (k <= 0 || n <= 0)
-			return result;
-		Node node;
-		int index = 1;
-		backtarning(result, node, index, k, n);
-		return result;
-    }
-};
+// https://leetcode.cn/problems/wiggle-subsequence/submissions/
+//class Solution {
+//public:
+//    int wiggleMaxLength(vector<int>& nums) {
+//        if (nums.empty())
+//            return 0;
+//        if (nums.size() == 1)
+//            return 1;
+//        int prediff = 0;
+//        int curdiff = 0;
+//        int result = 1;
+//        for (int i = 0; i < nums.size() - 1; i++)
+//        {
+//            curdiff = nums[i + 1] - nums[i];
+//            if ((curdiff > 0 && prediff <= 0)
+//                || (curdiff < 0 && prediff >= 0))
+//            {
+//                result++;
+//                prediff = curdiff;
+//            }
+//        }
+//        return result;
+//    }
+//};
