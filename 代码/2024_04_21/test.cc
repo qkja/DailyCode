91c09e9f886fa5c0c4624f4fac6533bc5a4d6a15
qkj <qkj1205@163.com>
2024-04-21 16:02:45 -0600

    贪心-买卖股票的最佳时机

--- 代码/2024_04_21/test.cc
+++ 代码/2024_04_21/test.cc
@@ -1,35 +1,56 @@
 #include <vector>
+#include <climits>
 using namespace std;
 // https://leetcode.cn/problems/longest-continuous-increasing-subsequence/
+// class Solution
+// {
+// public:
+//   int findLengthOfLCIS(vector<int> &nums)
+//   {
+//     if (nums.empty())
+//       return 0;
+//     int result = 0;
+//     int ret = 1;
+//     int left = 0;
+//     int right = left + 1;
+//     while (right < nums.size())
+//     {
+//       if (nums[right] > nums[right - 1])
+//       {
+//         ret++;
+//         right++;
+//       }
+//       else
+//       {
+//         result = std::max(result, ret);
+//         ret = 1;
+//         left = right;
+//         right = left + 1;
+//       }
+//     }
+//     return std::max(result, ret);
+//   }
+// };
+
+// https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
 class Solution
 {
 public:
-  int findLengthOfLCIS(vector<int> &nums)
+  int maxProfit(vector<int> &prices)
   {
-    if (nums.empty())
-      return 0;
-    int result = 0;
-    int ret = 1;
-    int left = 0;
-    int right = left + 1;
-    while (right < nums.size())
+    int preMin = INT_MAX;
+    int maxVal = 0;
+    for (int i = 0; i < prices.size(); ++i)
     {
-      if (nums[right] > nums[right - 1])
-      {
-        ret++;
-        right++;
-      }
-      else
-      {
-        result = std::max(result, ret);
-        ret = 1;
-        left = right;
-        right = left + 1;
-      }
+      // 计算差值
+      maxVal = std::max(prices[i] - preMin, maxVal);
+      // 跟新最小值
+      preMin = std::min(preMin, prices[i]);
     }
-    return std::max(result, ret);
+    return maxVal;
   }
 };
+
 int main()
 {
   return 0;
