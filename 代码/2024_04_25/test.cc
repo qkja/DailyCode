4573bcff362f422f3c5048f193c7dbd6f6c0a61a
qkja <qkj1205@163.com>
2024-04-25 20:51:23 +0800

    贪心- 分发饼干

---
+++ 代码/2024_04_25/test.cc
@@ -0,0 +1,36 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+//https://leetcode.cn/problems/assign-cookies/
+//class Solution {
+//public:
+//    int findContentChildren(vector<int>& g, vector<int>& s) {
+//      if(g.empty() || s.empty())
+//        return 0;
+//      std::sort(g.begin(), g.end());
+//      std::sort(s.begin(), s.end());
+//
+//      int begin = 0;
+//      for(int i = 0; i < s.size(); ++i)
+//      {
+//        if(begin < g.size() && s[i] >= g[begin])
+//        {
+//          ++begin;
+//        }
+//      }
+//      return begin;
+//    }
+//};
+
+
+//https://leetcode.cn/problems/optimal-division/
+class Solution {
+public:
+    string optimalDivision(vector<int>& nums) {
+      string result;
+      if(nums.size())
+        return result;
+
+    }
+};
