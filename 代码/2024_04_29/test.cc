71af6b02c4ce08eb0d82624cd91997d8af3fb5bf
qkja <qkj1205@163.com>
2024-04-29 20:44:57 +0800

    贪心- 单调递增的数字

--- 代码/2024_04_29/test.cc
+++ 代码/2024_04_29/test.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <vector>
 using namespace std;
 
@@ -117,27 +118,91 @@ using namespace std;
 //}
 
 
+//class Solution {
+//public:
+//    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+//      int n = gas.size();
+//
+//      for(int i = 0; i < n; ++i)
+//      {
+//        int rest = 0;
+//        int step = 0;
+//        for(; step < n; ++step)
+//        {
+//          int index = (i+step)%n;
+//          rest = rest + gas[index] - cost[index];
+//          if(rest < 0)
+//            break;
+//        }
+//        if(rest >= 0)
+//          return i;
+//        i += step;
+//      }
+//      return -1;
+//    }
+//};
+
+
+
+
+//https://leetcode.cn/problems/monotone-increasing-digits
+//class Solution {
+//public:
+//    int monotoneIncreasingDigits(int n) 
+//    {
+//      std::string str = std::to_string(n);
+//      for(int i = 1; i < str.size(); ++i)
+//      {
+//        if(str[i] < str[i-1])
+//        {
+//          // 可以更新;了,找第一个相似的
+//          for(int j = i-1; j >= 0; --j)
+//          {
+//            if(str[j] != str[i])
+//            {
+//              str[j] = str[j] -1;
+//              j++;
+//              for(;j < str.size(); j++)
+//                str[j] = '9';
+//              
+//              if(str[0] == '0')
+//                return std::stoi(str.erase(0,1));
+//              else 
+//                return stoi(str);
+//            }
+//          }
+//        }
+//      }
+//      return 0;
+//    }
+//};
+
+
+
 class Solution {
 public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-      int n = gas.size();
-
-      for(int i = 0; i < n; ++i)
+    int monotoneIncreasingDigits(int n) {
+      std::string str = to_string(n);
+      int i = 0;
+      for(i = 0; i < str.size()-1; ++i)
       {
-        int rest = 0;
-        int step = 0;
-        for(; step < n; ++step)
-        {
-          int index = (i+step)%n;
-          rest = rest + gas[index] - cost[index];
-          if(rest < 0)
-            break;
-        }
-        if(rest >= 0)
-          return i;
-        i += step;
+        if(str[i] > str[i+1])
+          break;
       }
-      return -1;
+      if(i == str.size() - 1)
+        return n;
+      int j = 0;
+      for(j = i - 1; j >=0; --j)
+      {
+        if(str[j] != str[i])
+          break;
+      }
+      // 将j+1 变换一下
+      str[j+1] = str[j+1] - 1;
+      for(j = j+2; j < str.size(); j++)
+        str[j] = '9';
+      if(str[0] == '0')
+        return stoi(str.erase(0, 1));
+      return stoi(str);
     }
 };
-
